9831661d48aa0d2756f2671577606aad9eb13319
micah20002 <52258583+micah20002@users.noreply.github.com>
2021-09-16 12:53:31 -0400

    Delete excercises/sep-14 directory

--- excercises/sep-14/while-loops1.py
+++
@@ -1,7 +0,0 @@
-command = input("Do you want to say hello? (Y/N)").lower().strip()
-
-while command == "y" :
-    print("Hello!")
-    command = input("Do you want to say hello again?")
-
-print("Goodbye")
--- excercises/sep-14/while-loops2.py
+++
@@ -1,5 +0,0 @@
-counter = 1
-
-while counter <= 10:
-    print(f"{counter}")
-    counter +=1 #counter = counter +1 substitude
